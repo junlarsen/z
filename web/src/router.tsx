445e15e48a0228be67ac5e939d60df22f7a36509
Mats Larsen <mats@jun.codes>
2024-01-07 20:41:09 +0100

    feat: add landing page application selector

--- web/src/router.tsx
+++ web/src/router.tsx
@@ -1,6 +1,14 @@
 import {
+  Route,
   createBrowserRouter,
   createRoutesFromElements,
 } from "react-router-dom";
+import { GearPage } from "./modules/gear/gear-page.tsx";
 
-export const router = createBrowserRouter(createRoutesFromElements(<></>));
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<GearPage />} />
+    </>,
+  ),
+);
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "^/api/.*": {
-        target: "http://localhost:3001/",
+        target: "http://localhost:8080/",
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
